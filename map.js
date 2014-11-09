4cd624f64f685a3b3eb497258d124d506e944d50
Tharik Keizer <tharik@Thariks-MacBook-Pro.local>
2014-11-09 13:22:50 +0530

    updated to support multiple graph node types

--- map.js
+++ map.js
@@ -27,20 +27,57 @@ $(document).ready(function(){
 					var source = $("#gui-input-source").val();
 					var target = $("#gui-input-target").val();
 					var counter = 0;
+					var nodes = [];
+
+
+					
+					res.results[0].data.forEach(function(row) {
+					//	OSRM.Geocoder.call(OSRM.C.VIA_LABEL, row["row"][0]["latitude"] + " , " + row["row"][0]["longitute"]);
+					//	
+					//	$("#resultText").append('<li>'+ counter + " - "+ row["graph"]["nodes"][0]["labels"][0] + " - " + row["row"][0]["name"]+'</li>');
+
+
+						row["graph"]["nodes"].forEach(function(graphRow){
+					
+							var id = graphRow["id"];
+							var label = graphRow["labels"][0];
+							var nodeName = graphRow["properties"]["name"];
+							var longitute = graphRow["properties"]["longitute"];
+							var latitude = graphRow["properties"]["latitude"];
+
+							nodes[counter] = '{"id" : '+id+', "label":"'+label+'", "name":"'+nodeName+'", "longitute":'+longitute+',"latitude":'+latitude+'}';
+
+							counter = counter + 1;
+						});
+					});
+
+
+
+					//Remove duplicate nodes
+					var uniqueNodes = [];
+					$.each(nodes, function(i, el){
+					    if($.inArray(el, uniqueNodes) === -1) uniqueNodes.push(el);
+					});
+
 
 					$("#resultText").empty();
 					$("#gui-reset").click();
-
 					OSRM.Geocoder.call(OSRM.C.SOURCE_LABEL, source);
-					
-					res.results[0].data.forEach(function(row) {
-						OSRM.Geocoder.call(OSRM.C.VIA_LABEL, row["row"][0]["latitude"] + " , " + row["row"][0]["longitute"]);
-						counter = counter + 1;
-						$("#resultText").append('<li>'+ counter + " - "+ row["graph"]["nodes"][0]["labels"][0] + " - " + row["row"][0]["name"]+'</li>');
+
+					counter = 0;
+					uniqueNodes.forEach(function(row){
+						counter = counter+ 1;
+						row = JSON.parse(row); 
+
+						$("#resultText").append('<li>'+ counter + " - "+ row["label"] + " - " + row["name"]+'</li>');
+						OSRM.Geocoder.call(OSRM.C.VIA_LABEL, row["latitude"] + " , " + row["longitute"]);
+
 					});
 
 					OSRM.Geocoder.call(OSRM.C.TARGET_LABEL, target);
 					$("#gui-input-target").change();
+
+					var x = "x";
 
 
 				}
